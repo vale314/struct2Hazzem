19b6dd04ac1c050a8455e7008cf450005c93163e
vale314 <electrovale314@hotmail.com>
2019-05-04 12:36:41 -0500

    Actividad 13

--- Actividad13/Haffman/hofmann.h
+++ Actividad13/Haffman/hofmann.h
@@ -31,7 +31,8 @@ public:
 
         void clear();
         bool empty() const;
-        void push(const T &elem,const int);
+        void push(const T &elem,const int,NodoHFF* izq=nullptr,NodoHFF* der=nullptr);
+        void createTree();
         T& operator[](size_t idx) const;
         size_t  size()const;
 };
@@ -58,20 +59,24 @@ bool LDL<T>::empty() const
 
 
 template<typename T>
-void LDL<T>::push(const T &elem,const int frec)
+void LDL<T>::push(const T &elem,const int frec,NodoHFF* izq,NodoHFF* der)
 {
+    //pasamos los caracteres a int
     int data=int(elem);
 
+    //si esta vacio solo agregamos
     if(empty())
     {
-        listFront = new NodoHFF(elem,frec,nullptr,nullptr);
+        listFront = new NodoHFF(elem,frec,nullptr,nullptr,izq,der);
         listBack = listFront;
     }
     else
     {
-
+        /*Comprobamos que la frecuencia sea menor que el primero o
+            que el primero tenga la misma frecuencia y sea menor nuestro new caracter
+        */
         if((frec<listFront->frecuencia)||(frec==listFront->frecuencia&&data<int(listFront->dato))){
-            NodoHFF* nuevo =new NodoHFF(elem,frec,nullptr,listFront);
+            NodoHFF* nuevo =new NodoHFF(elem,frec,nullptr,listFront,izq,der);
             listFront->anterior=nuevo;
             listFront=nuevo;
             listSize++;
@@ -81,6 +86,12 @@ void LDL<T>::push(const T &elem,const int frec)
         NodoHFF* aux;
         aux=listFront;
 
+        /*
+            Recorremos la lista mientras el siguiente elemento sea diferente de NULL
+                si frecuencia es mayor aumentamos
+                si no, si la frecuencia es igual y el dato es mas grande o igual (en su conversion int) aumentamos
+                si no rompemos
+        */
         while(aux->siguiente!=NULL){
             if(frec>aux->frecuencia)
                 aux=aux->siguiente;
@@ -91,20 +102,68 @@ void LDL<T>::push(const T &elem,const int frec)
                 break;
         }
 
+        /*
+         * MAYOR
+            si el elemento fue el ultimo que se verifico tenemos 2 posibilidades:
+                que sea mayor a ese elemento o menor|| ya sea su frecuencia o su data
+           si es mayor entramos
+        */
         if(aux->siguiente==NULL){
             if(frec>aux->frecuencia||(frec==aux->frecuencia&&data>=int(aux->dato))){
-                NodoHFF* nuevo =new NodoHFF(elem,frec,aux,nullptr);
+                NodoHFF* nuevo =new NodoHFF(elem,frec,aux,nullptr,izq,der);
                 aux->siguiente=nuevo;
             }
         }
-
+        /*
+         * MENOR
+            si el elemento no fue el ultimo elemento
+            validamos que sea menor a el actual  y lo ingresaremos en la parte de la izquierda
+            configuramos los nodos
+        */
         if((frec<aux->frecuencia)||(frec==aux->frecuencia&&data<=int(aux->dato))){
-            NodoHFF* nuevo =new NodoHFF(elem,frec,aux->anterior,aux);
+            NodoHFF* nuevo =new NodoHFF(elem,frec,aux->anterior,aux,izq,der);
             nuevo->anterior->siguiente=nuevo;
             nuevo->siguiente->anterior=nuevo;
         }
     }
     listSize++;
+}
+
+template<typename T>
+void LDL<T>::createTree()
+{
+    if(empty()){
+        cout<<"No Se Encuentran Elementos"<<endl;
+        return;
+    }
+
+    int frecuencia=0;
+
+    NodoHFF* aux = listFront;
+    frecuencia=aux->frecuencia;
+    frecuencia=frecuencia+aux->siguiente->frecuencia;
+
+    NodoHFF* izq=listFront;
+    NodoHFF* der=listFront->siguiente;
+
+    listFront->anterior=nullptr;
+    listFront->siguiente=nullptr;
+
+    listFront=der->siguiente;
+
+    der->anterior=nullptr;
+    der->siguiente=nullptr;
+
+    if(listFront!=NULL)
+        listFront->anterior=nullptr;
+
+    listSize=listSize-2;
+
+    push('\x01',frecuencia,izq,der);
+
+    if(listFront->siguiente!=NULL)
+        createTree();
+
 }
 
 template<typename T>
--- Actividad13/Haffman/main.cpp
+++ Actividad13/Haffman/main.cpp
@@ -10,27 +10,45 @@ int main()
 
     LDL<char> aux;
 
-    aux.push('L',1);
-    aux.push('a',6);
-    aux.push(' ',6);
-    aux.push('r',4);
-    aux.push('u',2);
-    aux.push('t',4);
-    aux.push('n',2);
-    aux.push('o',5);
-    aux.push('s',2);
-    aux.push('p',2);
-    aux.push('l',1);
+//    aux.push('L',1);
+//    aux.push('a',6);
+//    aux.push(' ',6);
+//    aux.push('r',4);
+//    aux.push('u',2);
+//    aux.push('t',4);
+//    aux.push('n',2);
+//    aux.push('o',5);
+//    aux.push('s',2);
+//    aux.push('p',2);
+//    aux.push('l',1);
+//    aux.push('.',1);
+//    aux.push('\xF3',1);
+
+    aux.push('V',1);
+    aux.push('I',2);
+    aux.push('L',2);
+    aux.push('N',2);
+    aux.push(' ',3);
+    aux.push('A',6);
+    aux.push('T',2);
     aux.push('.',1);
 
-    /*
-    aux.push('\x01',10);
-    aux.push('\x01',10);*/
 
     cout<<"Size: "<<aux.size()<<endl;
 
     for(size_t i=0;i<aux.size();i++){
         cout<<aux[i]<<endl;
     }
+
+    cout<<endl<<endl<<endl;
+
+    aux.createTree();
+
+    cout<<"Size: "<<aux.size()<<endl;
+
+    for(size_t i=0;i<aux.size();i++){
+        cout<<aux[i]<<endl;
+    }
+
     return 0;
 }
